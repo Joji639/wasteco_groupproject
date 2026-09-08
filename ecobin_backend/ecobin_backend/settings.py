ce424fd341e1732a900555dffa90a23cee0f9029
joji639 <jojijoj367@gmail.com>
2026-09-08 20:44:28 +0530

    remove the user code

--- ecobin_backend/ecobin_backend/settings.py
+++ ecobin_backend/ecobin_backend/settings.py
@@ -56,7 +56,6 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'drf_spectacular',
     'accounts',
-    'ecobinusers',
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
--- ecobin_backend/ecobinusers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EcobinusersConfig(AppConfig):
-    name = 'ecobinusers'
